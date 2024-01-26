67e2369848e6de2daac0d38929a9e38d49f096c6
Judicaël Grasset <judicael.grasset@meteo.fr>
2024-01-26 09:19:22 +0000

    add env script for nvhpc 23.11

---
+++ support/wrap/NVHPC2311/env.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export PATH=/opt/softs/nvidia/hpc_sdk/Linux_x86_64/23.11/compilers/bin:$PATH
+export CPATH=/opt/softs/nvidia/hpc_sdk/Linux_x86_64/23.11/comm_libs/11.8/hpcx/hpcx-2.14/ompi/include/:$CPATH
